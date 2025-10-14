5dac39c0286e1fd19055b9e1cb05fcabf35a2233
thota-vivek05 <thota.vivek2005@gmail.com>
2025-10-14 23:57:48 +0530

    Revert "verifier login"
    This reverts commit b24d8f76b2e260f2739173df7b562a0c9276ee7d.

--- model/Verifier.js
+++ model/Verifier.js
@@ -16,7 +16,7 @@ const verifierSchema = new mongoose.Schema({
         type: String,
         match: [/^\+?[\d\s-]{10,}$/, 'Please enter a valid phone number']
     },
-    password: { 
+    password_hash: {  // Changed from 'password' to 'password_hash'
         type: String,
         required: true
     },
